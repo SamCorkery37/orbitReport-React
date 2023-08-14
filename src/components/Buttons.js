da831da6a9ad21dcc42454380fe21a9b765035af
Sam Corkery <134240832+Piemaster37@users.noreply.github.com>
2023-08-14 17:45:26 -0500

    commit test

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -1,10 +1,19 @@
-const Buttons = () => {
+import satData from "./satData";
+import "./styling.css"
+
+const Buttons = ({filterByType, setSat, displaySats}) => {
   return (
     <div>
-      <button>Placeholder Button</button>
-      <button>All Orbits</button>
+      {displaySats.map((sat, id) => {
+        return (
+          <button onClick={() => filterByType(sat)} key={id}>
+          {sat} Orbit
+          </button> 
+        );
+      })}
+      <button onClick={() => setSat(satData)}>All Orbits</button>
       </div>
   );
 };
-
+//test commit for push
 export default Buttons;
